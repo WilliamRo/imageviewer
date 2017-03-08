e74a660fc996b0f58f0b4b96be72ea058e5bd2e4
William <willi4m@zju.edu.cn>
2017-03-08 21:25:22 +0800

    [M] modified some codes

--- @ImageViewer/view.m
+++ @ImageViewer/view.m
@@ -9,22 +9,19 @@ pct = [0.4, 0.8];
 %% Open a new customised figure
 % get screen size
 screenSize = get(0, 'ScreenSize');
-[screenWidth, screenHeight] = deal(screenSize(3), screenSize(4));
+screenSize = screenSize(3:4);
 % get image size
 imgSize = size(this.Data);
-[imgHeight, imgWidth] = deal(imgSize(1), imgSize(2));
+imgSize = imgSize(1:2);
 % open a new figure
 f = figure('Name', this.FigureName, 'NumberTitle', 'off', ...
     'ToolBar', 'none', 'MenuBar', 'none', 'Units', 'pixels');
 % adjust figure position and size
-ratio = max([imgWidth / screenWidth, imgHeight / screenHeight]);
+ratio = max(imgSize ./ screenSize);
 scale = 1;
 if ratio < pct(1), scale = pct(1) / ratio; end
 if ratio > pct(2), scale = pct(2) / ratio; end
-f.Position = [...
-    screenWidth / 2 - imgWidth * scale / 2, ...
-    screenHeight / 2 - imgHeight * scale / 2, ...
-    imgWidth * scale, imgHeight * scale];
+f.Position = [(screenSize - imgSize * scale) / 2, imgSize * scale];
 f.KeyPressFcn = @btnPressed;
 f.UserData = this;
 % set new axes
